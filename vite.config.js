89e5b3e546493b3ae4c7efc2e95de8469f319a71
SrGeneroso <5541794+SrGeneroso@users.noreply.github.com>
2023-11-24 17:39:37 +0100

    minor fix to caching

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,9 @@ export default defineConfig({
         'favicon.svg',
         'favicon.ico',
         'apple-touch-icon.png',
-        'mask-icon.svg'
+        'mask-icon.svg',
+        'screenshot1.png',
+        'screenshot2.png'
       ],
       manifest: manifestConfig
     })
