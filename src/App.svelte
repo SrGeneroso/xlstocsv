0d2b8cc513f23f684a370ee16200d0592d7e1609
SrGeneroso <5541794+SrGeneroso@users.noreply.github.com>
2023-11-26 23:22:49 +0100

    One button demo

--- src/App.svelte
+++ src/App.svelte
@@ -1,79 +1,44 @@
 <script>
-  import {
-    exportedJson,
-    data,
-    handleSpreadsheetFile,
-    spreadsheetConversion
-  } from './lib/Conversion'
+	import { exportedJson, data, handleSpreadsheetFile, spreadsheetConversion } from './lib/Conversion'
+	import Droplet from './lib/droplet.svelte'
 
-  function addToList(list, input) {
-    data[list] = [...data[list], data[input]]
-    data[input] = ''
-  }
-  function removeFromList(list, index) {
-    data[list].splice(index, 1)
-    data[list] = data[list]
-  }
+	function handleFiles(files) {
+		handleSpreadsheetFile(files)
+		console.log({ files })
+		for (const file of files) {
+			console.log(file.name)
+		}
+	}
 </script>
 
 <!-- Select File -->
-<input
-  type="file"
-  id="filePicker"
-  accept="application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-  on:change={handleSpreadsheetFile}
-  on:mouseenter={() => console.log(exportedJson === null)}
+<!-- <input
+	type="file"
+	id="filePicker"
+	accept="application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+	on:change={handleSpreadsheetFile}
+	on:mouseenter={() => console.log(exportedJson === null)}
 />
-<form on:submit|preventDefault={() => spreadsheetConversion()}>
-  <button type="submit">Export</button>
-</form>
 
-<!-- Input Forms -->
-{#each [['importList', 'importInput', 'import'], ['exportList', 'exportInput', 'export']] as list, index}
-  <form
-    on:submit|preventDefault={() =>
-      data[list[1]] ? addToList(list[0], list[1]) : ''}
-  >
-    <label for={`input${index}`}>Columns to {list[2]}</label>
-    <input
-      id={`input${index}`}
-      bind:value={data[list[1]]}
-      type="text"
-      placeholder="Column name"
-    />
-    <button type="submit">Add</button>
-  </form>
-{/each}
-
-<!-- List Views  -->
-<div class="lists">
-  {#each ['importList', 'exportList'] as list}
-    <div class="list">
-      {#each data[list] as item, index}
-        <div class="description">{item}</div>
-        <button class="button" on:click={() => removeFromList(list, index)}
-          >❌</button
-        >
-      {/each}
-    </div>
-  {/each}
-</div>
+<label for="filePicker" class="file-upload">
+	<span>Select File</span>
+	<input type="file" id="filePicker" accept="application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" />
+</label> -->
+<p>Conversion of Davasa to GDTaller</p>
+<Droplet
+	{handleFiles}
+	let:droppable
+	acceptedMimes={['application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet']}
+	maxFilesAllowed={1}
+>
+	<div class="zone" class:droppable>Select or drop files here</div>
+</Droplet>
 
 <style>
-  .list {
-    display: flex;
-    flex-wrap: wrap;
-    align-items: center;
-    padding: 1rem;
-    gap: 0.5rem;
-    border: 1px dotted #ffffff;
-  }
-  .button {
-    background-color: #454545;
-    align-self: center;
-    justify-self: right;
-  }
-  button:disabled {
-    cursor: not-allowed;
-  }
+	.zone {
+		border: 1px dotted red;
+		width: 50vw;
+		height: 3rem;
+		background-color: gray;
+	}
 </style>
---
+++ src/lib/droplet.svelte
@@ -0,0 +1,154 @@
+<!-- https://github.com/probablykasper/svelte-droplet/blob/master/src/lib/FileDrop.svelte -->
+<script lang="ts">
+	export let handleFiles: (files: File[]) => void = () => {
+		/* noop */
+	}
+
+	export let acceptedMimes: string[] | null = null
+	export let maxFilesAllowed = 0
+	export let disabled = false
+	export let name: string | null | undefined = undefined
+	export let tabindex = 0
+
+	let droppable = false
+	$: if (disabled) droppable = false
+
+	let input: HTMLInputElement
+
+	function getAcceptedFiles(files: FileList | File[] = []): File[] {
+		let acceptedFiles = []
+		for (let i = 0; i < files.length; i++) {
+			if (acceptedMimes === null || isAccepted(files[i])) {
+				acceptedFiles.push(files[i])
+			} else {
+				console.log('mime incorrect')
+				return
+			}
+		}
+		if (maxFilesAllowed !== 0) {
+			acceptedFiles = acceptedFiles.slice(0, maxFilesAllowed)
+		} else {
+			console.log('max files number reached')
+			return
+		}
+		return acceptedFiles
+	}
+
+	function isAccepted(item: DataTransferItem | File): boolean {
+		if (acceptedMimes === null) {
+			return true
+		}
+		for (const acceptedType of acceptedMimes) {
+			if (acceptedType.startsWith('.')) {
+				const file = item instanceof DataTransferItem ? item.getAsFile() : item
+				if (file?.name.endsWith(acceptedType)) {
+					return true
+				}
+			}
+			if (acceptedType === 'application/*' && item.type.startsWith('application/')) {
+				return true
+			} else if (acceptedType === 'audio/*' && item.type.startsWith('audio/')) {
+				return true
+			} else if (acceptedType === 'video/*' && item.type.startsWith('video/')) {
+				return true
+			} else if (acceptedType === 'image/*' && item.type.startsWith('image/')) {
+				return true
+			} else if (acceptedType === 'text/*' && item.type.startsWith('text/')) {
+				return true
+			} else if (item.type === acceptedType) {
+				return true
+			}
+		}
+		return false
+	}
+
+	function dragOver(e: DragEvent) {
+		if (disabled) {
+			return
+		}
+		const items = Array.from(e.dataTransfer?.items || [])
+		for (const item of items) {
+			if (item.kind === 'file' && isAccepted(item)) {
+				droppable = true
+				return
+			}
+		}
+	}
+
+	function dragLeave() {
+		droppable = false
+	}
+
+	function drop(e: DragEvent) {
+		if (disabled) {
+			return
+		}
+		const acceptedFiles = getAcceptedFiles(e.dataTransfer?.files)
+		if (acceptedFiles.length > 0) {
+			handleFiles(acceptedFiles)
+		}
+		droppable = false
+	}
+
+	let inputFiles: FileList
+	function handleChange() {
+		const acceptedFiles = getAcceptedFiles(inputFiles)
+		if (acceptedFiles && acceptedFiles.length > 0) {
+			handleFiles(acceptedFiles)
+		}
+	}
+
+	function keydown(e: KeyboardEvent) {
+		if (e.key === ' ' || e.key === 'Enter') {
+			e.preventDefault()
+			input.click()
+		}
+	}
+</script>
+
+<div
+	on:drop|preventDefault={drop}
+	on:dragover|preventDefault={dragOver}
+	on:dragenter|preventDefault={dragOver}
+	on:dragleave|preventDefault={dragLeave}
+	{tabindex}
+	on:keydown={keydown}
+	on:click={() => input.click()}
+	role="button"
+	aria-label="File Upload"
+>
+	<slot {droppable}>Upload</slot>
+</div>
+<input
+	type="file"
+	accept={acceptedMimes === null ? null : acceptedMimes.join(',')}
+	multiple={maxFilesAllowed !== 1}
+	bind:files={inputFiles}
+	on:change={handleChange}
+	bind:this={input}
+	{disabled}
+	{name}
+/>
+
+<!-- <style lang="sass">
+	div
+		cursor: pointer
+	[disabled]
+		cursor: default
+	input
+		display: none
+</style> -->
+
+<style>
+	div {
+		cursor: pointer;
+	}
+
+	[disabled] {
+		cursor: default;
+	}
+
+	input {
+		display: none;
+	}
+</style>
